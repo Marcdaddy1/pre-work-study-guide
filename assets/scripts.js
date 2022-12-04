bac864b2f994fd9547459c239009df8f44e7f93e
unknown <abbey.marcus@gmail.com>
2022-12-04 15:44:33 +0000

    added JavaScript to project

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,5 +1,19 @@
-var shape = "circle";
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-var topics = "HTML, CSS, Git, JavaScripts";
+if (topics === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topics === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topics === 'Git') {
+  console.log("Let's study Git!");
+} else if (topics === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}
 
-console.log(topics);
+var topics = ["HTML", "CSS", "Git", "JavaScript"];
+for(var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+   }
+   
